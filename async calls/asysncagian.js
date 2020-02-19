682501ea9f11ea83ac51ddc4e161d06ac9d47ef2
Avinash Gaurav <avinashgaurav711@gmail.com>
2020-02-19 23:22:54 +0530

    Asynchronous functionality Explanation

---
+++ async calls/asysncagian.js
@@ -0,0 +1,12 @@
+console.log("Starting the process");
+
+setTimeout(()=>{
+    console.log("timer of 2 secs");
+},2000);
+
+setTimeout(()=>{
+    console.log("timer of 0 secs");
+
+},0);
+
+console.log("This is the finishing step");
